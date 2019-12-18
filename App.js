6c97cc0f7f4c1fab0d2a46670e570edfbea5bb3f
wansiklee <dhkstlr2662@gmail.com>
2019-12-18 16:05:39 +0900

    Coding the UI part One

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+  Platform
+} from "react-native";
+
+const { height, width } = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar barStyle="light-content" />
+      <Text style={styles.title}>Kawai To Do</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.input} placeholder={"New To Do"} />
+      </View>
     </View>
   );
 }
@@ -12,8 +26,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F23657",
+    alignItems: "center"
   },
+  title: {
+    color: "white",
+    fontSize: 30,
+    marginTop: 50,
+    fontWeight: "200",
+    marginBottom: 30
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgba(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 8,
+        shadowOpacity: 0.6,
+        shadowRadius: 10,
+        shadowOffset: {
+          height: 4,
+          width: 0
+        }
+      }
+    })
+  }
 });
