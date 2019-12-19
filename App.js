13f3855397edda069cb61a8b01bd48a362fb60d3
wansiklee <dhkstlr2662@gmail.com>
2019-12-19 18:59:01 +0900

    Saving To Dos part One

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import {
 } from "react-native";
 import { AppLoading } from "expo";
 import ToDo from "./ToDo";
+import uuidv1 from "uuid/v1";
 
 const { height, width } = Dimensions.get("window");
 
@@ -62,8 +63,25 @@ export default class App extends React.Component {
   _addToDo = () => {
     const { newToDo } = this.state;
     if (newToDo !== "") {
-      this.setState({
-        newToDo: ""
+      this.setState(prevState => {
+        const ID = uuidv1();
+        const newToDoObject = {
+          [ID]: {
+            id: ID,
+            isCompleted: false,
+            text: newToDo,
+            createdAt: Date.now()
+          }
+        };
+        const newState = {
+          ...prevState,
+          newToDo: "",
+          toDos: {
+            ...prevState.toDos,
+            ...newToDoObject
+          }
+        };
+        return { ...newState };
       });
     }
   };
